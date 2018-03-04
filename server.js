fe655cb6d286b39628f0fa6f41e9e503b5a5a312
lightrow <lightrow@gmail.com>
2018-03-04 22:27:38 +0400

    render errors with ejs view engine

--- server.js
+++ server.js
@@ -15,8 +15,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use('/api/getplaces', require('./routes/api/getplaces'));
-app.use('*', require('./routes/error'));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -32,7 +35,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.end('error');
+  res.render('error', {title:"ERROR"});
 });
 
 module.exports = app;
